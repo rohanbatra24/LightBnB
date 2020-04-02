dcb3c654078beb9f1bb430262f4b1d692088378f
Rohan Batra <rohanbatra24@gmail.com>
2020-04-02 01:01:47 +0000

    added queries part 1

---
+++ 1_queries/1_get_user_details.sql
@@ -0,0 +1,3 @@
+select users.id, users.name, users.email, users.password
+from users
+where email='tristanjacobs@gmail.com';
---
+++ 1_queries/4_most_visited_cities.sql
@@ -0,0 +1,5 @@
+select properties.city as city, count(reservations) as total_reservations
+from properties join reservations
+  on properties.id = property_id
+group by city
+order by total_reservations desc;
